c0e1984bf488fa4f5aeeec8cd99e661c6a4f93bd
Kim HyunWoo <stories2@naver.com>
2018-12-21 13:50:14 +0900

    response as object not a string

--- functions/Utils/responseManager.js
+++ functions/Utils/responseManager.js
@@ -50,22 +50,22 @@ exports.flushResponse = function (response) {
 }
 
 exports.ok = function(response, msg) {
-    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_OK, msg)
+    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_OK, JSON.stringify(msg))
 }
 
 exports.badRequest = function(response, msg) {
-    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_BAD_REQUEST, msg)
+    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_BAD_REQUEST, JSON.stringify(msg))
 }
 
 exports.unauthorized = function(response, msg) {
-    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_UNAUTHORIZED, msg)
+    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_UNAUTHORIZED, JSON.stringify(msg))
 }
 
 exports.internalServerError = function(response, msg) {
-    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR, msg)
+    this.response(response, global.define.HEADERS_CONTENT_TYPE_APPLICATION_JSON, global.define.HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR, JSON.stringify(msg))
 }
 
-exports.response = function(response, type, code, msg) {
-    response.setHeader('Content-Type', type)
-    response.status(code).send(JSON.stringify(msg))
+exports.response = function(response, type, code, msgStr) {
+    response.setHeader(global.define.HEADERS_CONTENT_TYPE, type)
+    response.status(code).send(msgStr)
 }
