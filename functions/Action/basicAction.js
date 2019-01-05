b98ec8ab37d6d30e019e0c61e4e642e8be63820a
Kim HyunWoo <stories2@naver.com>
2019-01-05 23:58:39 +0900

    basicAction : say intro action

--- functions/Action/basicAction.js
+++ functions/Action/basicAction.js
@@ -1,3 +1,9 @@
 exports.hello = function (request, response, callbackFunc) {
+    const action = JSON.parse(JSON.stringify(request.action))
+    const responseManager = request.responseManager
     global.log.debug("basicAction", "hello", "user data: " + JSON.stringify(request.user) + " action data: " + JSON.stringify(request.action))
+
+    responseManager.pushTemplate(action["response"][global.define.DEFAULT_RESPONSE_TYPE_ZERO])
+    responseManager.pushQuickReply(action["quickReplies"])
+    callbackFunc()
 }
