4e073fa59ca298d1006eac5f9736b62500e0979d
Kim HyunWoo <stories2@naver.com>
2019-01-13 14:47:04 +0900

    hosting : add navigation menu to index

--- public/assets/js/Controllers/Shared/ToolbarController.js
+++ public/assets/js/Controllers/Shared/ToolbarController.js
@@ -2,4 +2,16 @@ app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidena
     KSAppService.printLogMessage("ToolbarController", "ToolbarController", "init", LOG_LEVEL_INFO);
 
     $scope.title = APP_TITLE
+    $scope.toggleLeft = buildToggler('left');
+
+    function buildToggler(navID) {
+        return function () {
+            // Component lookup should always be available since we are not using `ng-if`
+            $mdSidenav(navID)
+                .toggle()
+                .then(function () {
+                    KSAppService.printLogMessage("ToolbarController", "buildToggler", "toggle " + navID + " is done", LOG_LEVEL_DEBUG);
+                });
+        };
+    }
 })
