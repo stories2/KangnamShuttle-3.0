51028a1087d511b9d47fb90a54eae405d1b42527
Kim HyunWoo <stories2@naver.com>
2019-01-15 20:07:29 +0900

    httpRequestManager: create request bolierplate

--- functions/Utils/httpRequestManager.js
+++ functions/Utils/httpRequestManager.js
@@ -1,12 +1,27 @@
-exports.request = function (headerOptions, callbackFunc, data) {
+exports.request = function (headerOptions, successFunc, errorFunc, data) {
     const httpManager = require('http')
     global.log.debug("httpRequestManager", "request", "header: " + JSON.stringify(headerOptions))
 
-    httpManager.request(headerOptions, callbackFunc)
+    var callbackController = function(response) {
+        global.log.debug("httpRequestManager", "callbackController", "response code: " + response.statusCode + " headers: " + JSON.stringify(response.headers))
+
+        var responseStr = ""
+        response.on("data", function (chunk) {
+            responseStr += chunk
+        })
+
+        response.on("end", function () {
+            global.log.debug("httpRequestManager", "callbackController-end", "response: " + responseStr)
+
+            successFunc(responseStr)
+        })
+
+        response.on("error", function (error) {
+            global.log.error("httpRequestManager", "callbackController-error", "error accepted: " + JSON.stringify(error))
+        })
+    }
+
+    httpManager.request(headerOptions, callbackController)
     httpManager.write(data)
     httpManager.end()
-}
-
-exports.callback = function (response) {
-    
 }
