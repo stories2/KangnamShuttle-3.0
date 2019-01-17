4250e47f5acbbcd9a7c141c362ed3e4004993891
Kim HyunWoo <stories2@naver.com>
2019-01-17 15:46:11 +0900

    Hosting: ShuttleLocationController, show bus detail list data

--- public/assets/js/Controllers/view/ShuttleLocationController.js
+++ public/assets/js/Controllers/view/ShuttleLocationController.js
@@ -1,10 +1,16 @@
 app.controller("ShuttleLocationController", function ($scope, $http, $mdToast, $mdSidenav, $window, $timeout, $rootScope, KSAppService) {
     KSAppService.printLogMessage("ShuttleLocationController", "ShuttleLocationController", "init", LOG_LEVEL_INFO);
-    
+
+    $scope.busLocationData = {}
+    $scope.refreshTime = 0
+
     $scope.onLoad = function () {
+        $scope.refreshTime = SHUTTLE_LOCATION_REFRESH_TIME
     }
     
     $scope.stream = function (busLocationData) {
         KSAppService.info("ShuttleLocationController", "stream", "location data accepted")
+
+        $scope.busLocationData = busLocationData
     }
 })
