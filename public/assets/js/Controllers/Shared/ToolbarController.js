d80ac1aae10e6d8bdec474d99bfc7dc63e9f88eb
Kim HyunWoo <stories2@naver.com>
2019-01-19 13:16:09 +0900

    hosting: signIn form added

--- public/assets/js/Controllers/Shared/ToolbarController.js
+++ public/assets/js/Controllers/Shared/ToolbarController.js
@@ -1,4 +1,4 @@
-app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidenav, $window, KSAppService) {
+app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidenav, $window, $mdDialog, KSAppService) {
     KSAppService.printLogMessage("ToolbarController", "ToolbarController", "init", LOG_LEVEL_INFO);
 
     $scope.title = APP_TITLE
@@ -14,6 +14,21 @@ app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidena
         }
     ]
 
+    $scope.onBtnSignInClicked = function () {
+        $mdDialog.show({
+            controller: signInDialogController,
+            templateUrl: 'assets/view/dialog/signIn.html',
+            parent: angular.element(document.body),
+            // targetEvent: ev,
+            clickOutsideToClose:true
+        })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+    }
+
     function buildToggler(navID) {
         return function () {
             // Component lookup should always be available since we are not using `ng-if`
@@ -22,6 +37,25 @@ app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidena
                 .then(function () {
                     KSAppService.printLogMessage("ToolbarController", "buildToggler", "toggle " + navID + " is done", LOG_LEVEL_DEBUG);
                 });
+        };
+    }
+
+    function signInDialogController($scope, $mdDialog) {
+
+        $scope.data = {}
+
+        $scope.hide = function() {
+            $mdDialog.cancel();
+        };
+
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+
+        $scope.submit = function(isSignIn) {
+            $scope.data["signIn"] = isSignIn
+            KSAppService.debug("ToolbarController", "signInDialogController-submit", "user data: " + JSON.stringify($scope.data))
+            $mdDialog.hide($scope.data);
         };
     }
 })
