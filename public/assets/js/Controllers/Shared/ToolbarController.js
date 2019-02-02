76c53260bdaa90c8f6f41db59c038598b5da03eb
Kim HyunWoo <stories2@naver.com>
2019-02-02 19:07:39 +0900

    Hosting: ToolbarController, google sign in added

--- public/assets/js/Controllers/Shared/ToolbarController.js
+++ public/assets/js/Controllers/Shared/ToolbarController.js
@@ -181,6 +181,29 @@ app.controller("ToolbarController", function ($scope, $http, $mdToast, $mdSidena
             KSAppService.debug("ToolbarController", "signInDialogController-submit", "user data: " + JSON.stringify($scope.data))
             $mdDialog.hide($scope.data);
         };
+
+        $scope.googleSignIn = function () {
+            KSAppService.info("ToolbarController", "signInDialogController-googleSignIn", "start google sign in")
+            var provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+                KSAppService.showToast(user.displayName + "님 안녕하세요", TOAST_SHOW_LONG)
+            }).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+                KSAppService.showToast("이런, 계정 연동 중 문제가 발생해버렸군요. " + errorMessage, TOAST_SHOW_LONG)
+            });
+        }
     }
 
     listenAuthStatusChanged()
