20d38dd9279789f817de794eb8cd45d1b382ebce
Kim HyunWoo <stories2@naver.com>
2019-02-14 22:05:25 +0900

    Hosting: publicTransportBus, show bus arrive info as list

--- public/assets/js/Controllers/view/PublicTransportBusController.js
+++ public/assets/js/Controllers/view/PublicTransportBusController.js
@@ -1,7 +1,11 @@
 app.controller("PublicTransportBusController", function ($scope, $http, $mdToast, $mdSidenav, $window, $mdDialog, KSAppService) {
+
+    $scope.platformAndRoute = {}
+
     KSAppService.info("PublicTransportBusController", "PublicTransportBusController", "init");
 
     $scope.streamBus = function (platformAndRoute) {
         KSAppService.debug("PublicTransportBusController", "streamBus", "data: " + JSON.stringify(platformAndRoute))
+        $scope.platformAndRoute = platformAndRoute
     }
 })
