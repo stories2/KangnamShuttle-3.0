61e0a6770c26d447ed3e82b50edf2851d726c207
Kim HyunWoo <stories2@naver.com>
2019-03-01 17:55:37 +0900

    Hosting: ttsGenerate, page layout updated

--- public/assets/js/Controllers/view/TtsGenerateController.js
+++ public/assets/js/Controllers/view/TtsGenerateController.js
@@ -1,3 +1,10 @@
 app.controller('TtsGenerateController', function ($scope, $http, $mdToast, $mdSidenav, $window, $mdDialog, KSAppService) {
     KSAppService.info('TtsGenerateController', 'TtsGenerateController', 'init')
+
+    $scope.memeList = [
+        {
+            mid: "test",
+            name: "name"
+        }
+    ]
 })
