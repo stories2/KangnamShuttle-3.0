e8cf840e5394143e35d893b9429a3caf3c5cc6a6
Kim HyunWoo <stories2@naver.com>
2020-01-22 14:27:14 +0900

    검색결과가 여러개일 경우 댓글을 여러번에 나누어서 작성하도록 수정

--- functions/Route/DB/everytimeTrigger.js
+++ functions/Route/DB/everytimeTrigger.js
@@ -4,28 +4,30 @@ exports.articleCreated = (snapshot, context) => {
     const schoolManager = require('../../Core/schoolManager')
     const everytimeManager = require('../../Core/everytimeManager')
 
-    
-
     if (article && article.title) {
         const titleOrder = article.title.split(" ")
         if (titleOrder.length > 1 && titleOrder[0] === '교직원') {
             
             schoolManager.searchSchoolWorkerByName(titleOrder[1], (workerList) => {
-                let commentMsg = '';
+                let commentMsg = '[강남대학교 달구지봇]\n';
                 if (!workerList || Object.keys(workerList).length <= 0) {
-                    commentMsg = `교직원 ${titleOrder[1]} 검색 결과 0건\n`;
+                    commentMsg += `교직원 ${titleOrder[1]} 검색 결과 0건\n`;
+                    everytimeManager.routineOfAddComment(context.params.articleID, commentMsg);
                 } else {
                     const workerKeyList = Object.keys(workerList);
-                    commentMsg = `교직원 ${titleOrder[1]} 검색 결과 ${workerKeyList.length}건\n`;
+                    commentMsg += `교직원 ${titleOrder[1]} 검색 결과 ${workerKeyList.length}건\n`;
                     workerKeyList.forEach(key => {
                         // const worker = 
                         const worker = workerList[key];
-                        commentMsg += `----------------\n소속: ${worker.area}\n성함: ${worker.name}\n연락처: ${worker.tel}\n이메일: ${worker.email}\n위치: ${worker.location}`
+                        const resultMsg = commentMsg + `<소속: ${worker.area}\n성함: ${worker.name}\n연락처: ${worker.tel}\n이메일: ${worker.email}\n위치: ${worker.location}> `
+                        everytimeManager.routineOfAddComment(context.params.articleID, resultMsg);
                     })
                 }
-                console.log('comment msg: ', commentMsg)
+                // console.log('comment msg: ', commentMsg)
             })
         }
+    } else {
+
     }
     return snapshot.val();
 }
